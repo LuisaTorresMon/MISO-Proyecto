40db5a112eda4d4c9682383788622bb9e888e6f4
LuisaTorresMon <lj.torresm1@uniandes.edu.co>
2024-09-11 23:49:17 -0500

    Avance en el MS de Planes

---
+++ plans/src/commands/create.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timedelta
+from flask import jsonify, request
+import requests
+from .base_command import BaseCommannd
+from src.models.model import Contract, Plan, db, ContractSchema, ContractGetJsonSchema
+from src.utils.utils import FileUtils
+
+contract_schema = ContractSchema()
+
+class Create(BaseCommannd):
+    
+    def __init__(self, data_contract):
+        self.plan_id = data_contract.get('plan_id')
+        self.empresa_id = data_contract.get('empresa_id')
+
+    def execute(self):
+
+        plan = Plan.query.filter_by(id=self.plan_id).first()
+        if not plan:
+            return jsonify({"error": "El plan no existe"}), 404
+
+        new_contract = Contract (
+            es_activo = True,
+            plan_id = self.plan_id,
+            empresa_id = self.empresa_id
+        )
+
+        db.session.add(new_contract)
+        db.session.commit()
+
+        schema = ContractGetJsonSchema()
+        contract_data = schema.dump(new_contract)
+
+        return contract_data
--- plans/src/models/model.py
+++ plans/src/models/model.py
@@ -1,5 +1,5 @@
 import uuid
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import declarative_base
@@ -14,7 +14,7 @@ db = SQLAlchemy()
 class Plan(db.Model):
     __tablename__ = 'plan'
 
-    id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    id = db.Column(db.Integer, primary_key=True)
     nombre_plan = db.Column(db.String(100))
     precio = db.Column(db.Integer)
 
@@ -25,19 +25,19 @@ class PlanSchema(SQLAlchemyAutoSchema):
         load_instance = True
         include_fk = True
 
-    id = fields.String()
+    id = fields.Integer()
 
 class PlanGetJsonSchema(SQLAlchemyAutoSchema):
-    id = fields.String()
+    id = fields.Integer()
     nombre_plan = fields.String()
     precio = fields.Integer()
 
 def cargar_datos_iniciales():
     if Plan.query.count() == 0:
         planes = [
-            Plan(nombre_plan="Plan Emprendedor", precio=100),
-            Plan(nombre_plan="Plan Empresario", precio=200),
-            Plan(nombre_plan="Plan Empresario Plus", precio=300)
+            Plan(id=1, nombre_plan="Plan Emprendedor", precio=100),
+            Plan(id=2, nombre_plan="Plan Empresario", precio=200),
+            Plan(id=3, nombre_plan="Plan Empresario Plus", precio=300)
         ]
         # Agregar los planes a la sesión
         db.session.bulk_save_objects(planes)
@@ -46,3 +46,29 @@ def cargar_datos_iniciales():
     else:
         print("La tabla Plan ya tiene datos")
 
+class Contract(db.Model):
+    __tablename__ = 'contract'
+
+    id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    fecha_inicio_plan = db.Column(db.DateTime, default=datetime.utcnow)
+    fecha_fin_plan = db.Column(db.DateTime, default=lambda: datetime.utcnow() + timedelta(days=365))
+    es_activo = db.Column(db.Boolean)
+    plan_id = db.Column(db.Integer)
+    empresa_id = db.Column(db.String(100))
+
+class ContractSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Contract
+        include_relationships = True
+        load_instance = True
+        include_fk = True
+
+    id = fields.String()
+
+class ContractGetJsonSchema(SQLAlchemyAutoSchema):
+    id = fields.String()
+    fecha_inicio_plan = fields.DateTime()
+    fecha_fin_plan = fields.DateTime()
+    es_activo = fields.Boolean()
+    plan_id = fields.String()
+    empresa_id = fields.String()
