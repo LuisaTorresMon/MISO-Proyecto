a60b4f1dfe9fe7f159b001e764f22c3577fa6f09
Jorge A. Romero Gutiérrez <ja.romerog12@uniandes.edu.co>
2024-10-09 00:22:05 -0500

    Ajuste en el token

--- users/src/blueprints/operations.py
+++ users/src/blueprints/operations.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, make_response, request, Blueprint
 from ..service.service import UserService
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
 
 users_blueprint = Blueprint('users', __name__)
 user_service = UserService()
@@ -21,7 +21,8 @@ def signIn():
 @users_blueprint.route("/auth/validate-token", methods=["POST"])
 @jwt_required()
 def validate_token():
-    return make_response({"msg": "OK"}), 200
+    current_jwt = get_jwt()
+    return make_response(current_jwt), 200
 
 
 # Consultar la salud del microservicio
--- users/src/service/service.py
+++ users/src/service/service.py
@@ -3,7 +3,7 @@ import bcrypt
 from datetime import datetime, timedelta
 from flask import jsonify
 from flask_jwt_extended import jwt_required, create_access_token, get_current_user, get_jwt
-from ..errors.errors import IncorrectUserOrPasswordException, UserAlreadyExistException
+from ..errors.errors import IncorrectUserOrPasswordException, UserAlreadyExistException, BadRequestException
 user_schema = UserSchema()
 
 class UserService():
@@ -12,8 +12,31 @@ class UserService():
         pass
 
     def create_user(self, user):
+        self.id_persona = user.get('id_person')
+        self.id_empresa = user.get('id_company')
         self.username = user.get('username')
         self.password = user.get('password').encode('utf-8')
+
+        if self.id_persona is None and self.id_empresa is None:
+            raise BadRequestException
+        elif self.id_persona is not None and self.id_empresa is not None:
+            raise BadRequestException
+        else:
+            if self.id_persona is not None:
+                if str(self.id_persona).strip() != "":
+                    try:
+                        int( self.id_persona)
+                    except ValueError:
+                        raise BadRequestException
+
+                if self.id_empresa is not None:
+                    if str(self.id_empresa).strip() != "":
+                        try:
+                            int( self.id_empresa)
+                        except ValueError:
+                            raise BadRequestException
+
+
 
         user = User.query.filter_by(nombre_usuario=self.username).first()
 
@@ -24,6 +47,8 @@ class UserService():
         hashed_password = bcrypt.hashpw(self.password, salt)
 
         new_user = User(
+            id_persona=self.id_persona,
+            id_empresa=self.id_empresa,
             nombre_usuario=self.username,
             contrasena=hashed_password.decode('utf-8')
         )
@@ -43,18 +68,16 @@ class UserService():
         if user is None:
             raise IncorrectUserOrPasswordException
 
-        # Verificar la contraseña
-        # No es necesario codificar user.contrasena nuevamente
         if not bcrypt.checkpw(password.encode('utf-8'), user.contrasena.encode('utf-8')):
             raise IncorrectUserOrPasswordException
 
-        # Generar el token JWT
-        additional_claims = {"usuario": "usuario"}
+        additional_claims = {
+            "id": user.id,
+            "id_person": user.id_persona,
+            "id_company": user.id_empresa
+        }
         token_de_acceso = create_access_token(identity=user.id, additional_claims=additional_claims)
 
         return {
-            "mensaje": "Inicio de sesión exitoso",
-            "token": token_de_acceso,
-            "id": user.id,
-            "username": user.nombre_usuario  # Corrige esto si el atributo es 'nombre_usuario'
+            "token": token_de_acceso
         }
